889ef3caa707f458dd5a962843a0d707e782c8a5
Zaniar Shokati <zaniar.shokati@gmail.com>
2023-10-14 22:57:42 +0200

    add testing

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
-from utilities import Visualization, ImageProcessing, CreateModel
+from utilities import Visualization, ProcessData, HandleModel
 
-from glob import glob
+
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 
@@ -10,80 +10,50 @@ AUTO = tf.data.experimental.AUTOTUNE
 import warnings
 
 warnings.filterwarnings("ignore")
+class Application:
+	def __init__(self) -> None:
+		self.visualize = Visualization()
+		self.data_processor = ProcessData()
+		self.model_handler = HandleModel()
 
+	def main(self):
+		# Load and preprocess data
+		train_path = "dataset_2/train/*/*.jpg"
+		test_path = "dataset_2/test/*/*.jpg"
+		df_train = self.data_processor.load_train_data(train_path)
+		df_test = self.data_processor.load_test_data(test_path)
+		# Process the test data
+		test_ds = self.data_processor.process_test_data(df_test)
+		# Show class distribution and class sample (optional)
+		# visualize.show_class_distribution(df)
+		# visualize.show_class_sample(df)
+		
+		# Process data and create training and validation datasets
+		train_ds, val_ds = self.data_processor.process_data(df_train)
 
-def main():
-    # Initialize the visualization object
-    visualize = Visualization()
+		# Create and compile the model
+		
+		model = self.model_handler.create_model()
+		model = self.model_handler.compile_model(model)
+		
+		print(model.summary())
 
-    # Load and preprocess data
-    df = load_data()
+		# Train the model and save the training history
+		# history = model.fit(train_ds, validation_data=val_ds, epochs=5, verbose=1)
+		history = self.model_handler.train_model(model, train_ds, val_ds)
+		# Display loss and AUC graphs
+		self.visualize.show_loss_val(history)
+		self.visualize.show_AUC_val(history)
+	
+		# Load the trained model
+		model = tf.keras.models.load_model("Model.h5")
 
-    # Show class distribution and class sample (optional)
-    # visualize.show_class_distribution(df)
-    # visualize.show_class_sample(df)
+		# Make predictions on the test data
+		predictions = model.predict(test_ds)
 
-    # Process data and create training and validation datasets
-    train_ds, val_ds = process_data(df)
-
-    # Create and compile the model
-    model = CreateModel.create_model()
-    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["AUC"])
-    print(model.summary())
-
-    # Train the model and save the training history
-    history = model.fit(train_ds, validation_data=val_ds, epochs=15, verbose=1)
-
-    # Display loss and AUC graphs
-    visualize.show_loss_val(history)
-    visualize.show_AUC_val(history)
-
-
-def load_data():
-    # Get a list of image file paths
-    images = glob("dataset_2/train/*/*.jpg")
-    # Replace backslash with forward slash to avoid unexpected errors
-    images = [path.replace("\\", "/") for path in images]
-
-    # Create a DataFrame with file paths, labels, and binary labels
-    df = pd.DataFrame({"filepath": images})
-    df["label"] = df["filepath"].str.split("/", expand=True)[2]
-    df["label_bin"] = np.where(df["label"].values == "malignant", 1, 0)
-
-    return df
-
-
-def process_data(df):
-    # Split data into features and targets
-    features = df["filepath"]
-    target = df["label_bin"]
-
-    # Split the data into training and validation sets
-    X_train, X_val, Y_train, Y_val = train_test_split(
-        features, target, test_size=0.15, random_state=10
-    )
-
-    # Initialize the image processing object
-    decoder = ImageProcessing()
-
-    # Create training dataset
-    train_ds = (
-        tf.data.Dataset.from_tensor_slices((X_train, Y_train))
-        .map(decoder.decode_image, num_parallel_calls=AUTO)
-        .batch(32)
-        .prefetch(AUTO)
-    )
-
-    # Create validation dataset
-    val_ds = (
-        tf.data.Dataset.from_tensor_slices((X_val, Y_val))
-        .map(decoder.decode_image, num_parallel_calls=AUTO)
-        .batch(32)
-        .prefetch(AUTO)
-    )
-
-    return train_ds, val_ds
-
+		df_test["predictions"] = predictions  # Add a new column for predictions
+		df_test.to_csv("test_predictions.csv", index=False)
 
 if __name__ == "__main__":
-    main()
+    app = Application()
+    app.main()
